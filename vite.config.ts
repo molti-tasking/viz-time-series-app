901ba21f2a448dcfd88351e119ab67d38d5fd826
Anton Wolter <tech@antonwolter.de>
2024-10-21 13:23:10 +0200

    Attempt to fix build and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 /**
  * @see https://vitejs.dev/config/
  */
 export default defineConfig({
-  base: "/viz-time-series-app", // Adjust this if deploying to a subdirectory
   plugins: [react(), eslintPlugin()],
   resolve: {
     alias: {
