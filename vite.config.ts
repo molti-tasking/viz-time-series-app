40f7c59f572d71cf74eda8054873320ddd749940
Anton Wolter <tech@antonwolter.de>
2024-10-22 15:40:23 +0200

    prepare gh pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path";
  * @see https://vitejs.dev/config/
  */
 export default defineConfig({
-  base: "/viz-time-series-app", // is being required to deploy it to gh pages
+  // base: "/viz-time-series-app", // is being required to deploy it to gh pages
   plugins: [react(), eslintPlugin()],
   resolve: {
     alias: {
