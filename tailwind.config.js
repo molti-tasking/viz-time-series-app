c36a6828500a47ea1938487a5d939cf1355cfbcb
Anton Wolter <tech@antonwolter.de>
2024-11-25 12:07:00 +0100

    small fix for local dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ["class"],
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
@@ -7,56 +9,58 @@ export default {
       padding: {
         DEFAULT: "1rem",
       },
+      git config --global user.email "tech@antonwolter.de"
+      git config --global user.name "Anton Wolter"
     },
-    extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
+extend: {
+  borderRadius: {
+    lg: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+  colors: {
+    background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+      DEFAULT: "hsl(var(--card))",
+        foreground: "hsl(var(--card-foreground))",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+    popover: {
+      DEFAULT: "hsl(var(--popover))",
+        foreground: "hsl(var(--popover-foreground))",
         },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+    primary: {
+      DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+    secondary: {
+      DEFAULT: "hsl(var(--secondary))",
+        foreground: "hsl(var(--secondary-foreground))",
         },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+    muted: {
+      DEFAULT: "hsl(var(--muted))",
+        foreground: "hsl(var(--muted-foreground))",
         },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+    accent: {
+      DEFAULT: "hsl(var(--accent))",
+        foreground: "hsl(var(--accent-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+    destructive: {
+      DEFAULT: "hsl(var(--destructive))",
+        foreground: "hsl(var(--destructive-foreground))",
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
+    border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
+          chart: {
+      1: "hsl(var(--chart-1))",
+        2: "hsl(var(--chart-2))",
           3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
+            4: "hsl(var(--chart-4))",
+              5: "hsl(var(--chart-5))",
         },
-      },
-    },
   },
-  plugins: [require("tailwindcss-animate")],
+},
+  },
+plugins: [tailwindcssAnimate],
 };
